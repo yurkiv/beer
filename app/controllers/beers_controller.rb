443de66877bbf4f5d3b0a20fd16b8b686187d923
Yurkiv Mykhailo <jurkiw.misha@gmail.com>
2022-10-24 20:12:46 +0300

    add search

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,18 +1,5 @@
 class BeersController < ApplicationController
   def index
-    @beers = PunkApi::Client.new.beers(request_params)
-
-    respond_to do |format|
-      format.html { }
-      format.turbo_stream { }
-    end
-  end
-
-  private
-
-  def request_params
-    request_params = { per_page: 10 }
-    request_params[:beer_name] = params[:query] if params[:query].present?
-    request_params
+    @beers = PunkApi::Client.new.beers(beer_name: params[:query], per_page: 10)
   end
 end
--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
             this.element.requestSubmit();
-        }, 200);
+        }, 1000);
     }
   }
 }
